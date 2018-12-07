12c90a7bb4f388eccb6f68123b086741b7614c27
Aditya Prerepa <43913660+AdiBaby@users.noreply.github.com>
2018-12-06 23:02:50 -0800

    Update MySQL_Connector.ino

--- MySQL_Connector.ino
+++ MySQL_Connector.ino
@@ -1,32 +1,40 @@
- //All libraries used
+/*
+ * (C) Aditya Prerepa 12-6-18
+ * THE SOLE PURPOSE OF THIS PROJECT IS GOOGLE CODE IN
+ */
+
+// output 5 goes to D1.
+// output 4 goes to D2.
+// output 3 goes to  ??
+// output 2 is builtin and D4 (cant be input)
+// Designers of this chip didn't map the pins correctly...
+
  #include <ESP8266WiFi.h>
  #include <WiFiClient.h>
  #include <ESP8266Ping.h>
  #include <MySQL_Connection.h>
  #include <MySQL_Cursor.h>
  #include <time.h>
-
- //Global variables
- // My MySQL server IP. 
- const IPAddress remote_ip(10,0,1,157);
+ 
+ const IPAddress remote_ip(10,0,1,157); 
+ int inputPirPin = 5;  
+ // Connect PIR to D1 in this case.
  // All users and passwords of the database and your wifi.
- char* DatabaseUser = "------";         
- char* DatabasePassword = "-----"; 
+ char* DatabaseUser = "-----";         
+ char* DatabasePassword = "--------"; 
  char* WifiSsid = "-------";
  char* WifiPass = "------";
-// Time zone. In California, it is 4. 
-// Change yours to whatever you need.
- int zone = 4 * 3600;
-// Always 0.
+ int zone = 4 * 3600; // Im in california
  int dst = 0;
-// Init the char * that will be inserted into mysql.
  char* exe_SQL = "";
-
+ int val = 0;
+ int pirState = LOW;
+ int ledPin = 2;
+ 
 //Define function names and signatures.
 void executeQuery(char*, MySQL_Connection);
 void updateTime(int, int);
  
-// Define WifiClient and the MYSQL connection itself.
  WiFiClient client;
  MySQL_Connection conn((Client *)&client);
  
@@ -41,19 +49,30 @@ void updateTime(int, int);
  // to establish the connection to the database. Takes in 
  // params the char* of DatabaseUser and DatabasePassword, 
  // as well as the MYSQL_Connection that was established above.
+ // Mode Pin inputPirPin to INPUT.
  void setup() {
    Serial.begin(115200);
    connectToWifi(WifiSsid, WifiPass);
    pingDatabase(remote_ip);
    configTime(zone, dst, "pool.ntp.org", "time.nist.gov");
    connectToDatabase(DatabaseUser, DatabasePassword, conn);
+   pinMode(inputPirPin, INPUT);
+   Serial.println(WiFi.localIP());
  }
- // Loop, runs forever. All this does is call updateTime, which updates
- // the time and also calls the executeQuery() function to update the database
- // itself. It updates every ten seconds, as seen by the delay
-void loop() {
-  updateTime(zone, dst);
-  delay(10000);
+
+// function to updtate the time and commit it to the database. Takes in the 
+// parameters of your current zone, and the dst
+void updateTime(int zone, int dst) {
+  char INSERT_SQL[1024];
+  time_t now = time(nullptr);
+  sprintf(INSERT_SQL, "INSERT INTO test_arduino.arduino_update (time_of_entry) VALUES (' " "%s" " ')", ctime(&now));
+  Serial.printf("%s; time:%s", INSERT_SQL, ctime(&now));
+  executeQuery(INSERT_SQL, conn);
+ }
+ 
+ // Loop, simple, one function.
+ void loop() {
+  checkPir();
  }
 
  // pingDatabase() takes in the mysql address and tries
@@ -89,7 +108,7 @@ void loop() {
  // DatabaseUser, Password for user, and the MYSQL_Connection object. 
  // This will try to connect to the IP on the MySQL port 3306 and will
  // print dots until then, as this is not a blocking function.
- void connectToDatabase(char* DatabaseUser, char* DatabasePassword, MySQL_Connection conn) {   
+ void connectToDatabase(char* DatabaseUser,char* DatabasePassword, MySQL_Connection conn) {   
   Serial.println("DB - Connecting...");
    while (conn.connect(remote_ip, 3306, DatabaseUser, DatabasePassword) != true) {
      delay(500);
@@ -104,16 +123,29 @@ void loop() {
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
   cur_mem->execute(INSERT_SQL);
  }
- 
- // function to updtate the time and commit it to the database. Takes in the 
- // parameters of your current zone, and the dst.
- void updateTime(int zone, int dst) {
-  char INSERT_SQL[1024];
-  time_t now = time(nullptr);
-  sprintf(INSERT_SQL, "INSERT INTO test_arduino.hello_arduino (message) VALUES (' " " %s " " , " "%s" "')", "Hey ddududude!", ctime(&now));
-  Serial.printf("%s; time:%s", INSERT_SQL, ctime(&now));
-  executeQuery(INSERT_SQL, conn);
- }
 
+ // checkPir() will update the value of the motion state. 
+ //If a value is 1, it will update the state and will call the updateTime()
+ // function, which calls executeQuery. Everything is happenening here.
+ void checkPir() {
+  val = digitalRead(inputPirPin);  
+  
+  if (val == HIGH) {            
+    digitalWrite(ledPin, HIGH);  
+    if (pirState == LOW) { 
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+      // Update Time to databse.
+      updateTime(zone,dst);
+    }
+  } else { 
+    digitalWrite(ledPin, LOW); 
+    if (pirState == HIGH){
+      Serial.println("Motion ended!");
+      pirState = LOW;
+    }
+  }
+  
+ }
 
 
